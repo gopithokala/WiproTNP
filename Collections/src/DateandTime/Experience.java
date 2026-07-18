8e97b84dc9f23d37a0a68f2cd7c75d3443150fd7
gopithokala <23BQ1A12D1@vvit.net>
2026-07-18 12:34:45 +0530

    Add files via upload

---
+++ Collections/src/DateandTime/Experience.java
@@ -0,0 +1,19 @@
+package DateandTime;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Experience {
+
+    public static void main(String[] args) {
+        LocalDate joiningDate = LocalDate.of(2024, 1, 15);
+
+        LocalDate today = LocalDate.now();
+
+        Period p = Period.between(joiningDate, today);
+
+        System.out.println("Experience:");
+        System.out.println(p.getYears() + " Years "
+                + p.getMonths() + " Months "
+                + p.getDays() + " Days");
+    }
