237f2763667a7056fd1d3aa90c72dc189e71a125
gopithokala <23BQ1A12D1@vvit.net>
2026-07-22 14:05:47 +0530

    Add files via upload

---
+++ Junit/src/com/wipro/test/TestSort.java
@@ -0,0 +1,21 @@
+package com.wipro.test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+import com.wipro.task.DailyTasks;
+
+public class TestSort {
+
+    @Test
+    public void testSortValues() {
+
+        DailyTasks dt = new DailyTasks();
+
+        int[] input = {5, 2, 8, 1, 4};
+        int[] expected = {1, 2, 4, 5, 8};
+
+        assertArrayEquals(expected, dt.sortValues(input));
+    }
+}
---
+++ Junit/src/com/wipro/test/TestStringConcat.java
@@ -0,0 +1,16 @@
+package com.wipro.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.wipro.task.DailyTasks;
+
+public class TestStringConcat {
+
+    @Test
+    public void testDoStringConcat() {
+        DailyTasks dt = new DailyTasks();
+        assertEquals("Hello World", dt.doStringConcat("Hello", "World"));
+    }
+}
